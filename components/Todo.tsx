d57567fb61640f80c2d1c989532f71e54886a1a2
m.ameen <ameen55668@gmail.com>
2022-12-21 19:00:28 +0500

    depl

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,75 +1,112 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDoc, getDocs } from "firebase/firestore";
 import img1 from "/Images/edit.png";
 import img2 from "/Images/cancel (1).png";
 import Image from "next/image";
+import { db } from "../config/Firebase";
 
 export default function Home() {
-  const [data, setdata] = useState<string[]>(["hallo this is my first task"]);
-  const [input, setinput] = useState<string>("");
-  const [doneTodos, setDoneTodos] = useState<string[]>([]);
-
-  const save = () => {
-    if (input.length > 0) {
-      let newdata: string[] = [...data, input];
-      setdata(newdata);
-      setinput("");
-    } else {
-      alert("Invalid");
+  const [ldg, setldg] = useState<any>("");
+  const [data, setdata] = useState<any>([]);
+  const [userInput, setUserInput] = useState<any>({
+    description: "",
+    id: data.length + 1,
+  });
+  const [doneTodos, setDoneTodos] = useState<any>([]);
+  const save = async (e: any) => {
+    try {
+      await addDoc(collection(db, "UserData"), userInput);
+      console.log(e.target);
+      e.target.disabled = true;
+    } catch (err) {
+      alert(err);
+    } finally {
+      e.target.disabled = false;
     }
+    setdata([...data, userInput]);
+    setUserInput({
+      description: "",
+      id: "",
+    });
   };
 
   let checked = (e: string) => {
-    let donedata: string[] = [...doneTodos, e];
-    setDoneTodos(donedata);
-    let filteredarr = data.filter((item) => {
-      return item != e;
-    });
-    setdata(filteredarr);
+    console.log(data);
+    console.log(e);
   };
 
   let cancel = (e: string) => {
-    let filteredarr = data.filter((item) => {
-      return item != e;
-    });
-    setdata(filteredarr);
+    console.log(e);
   };
 
-  let reset = () => {
-    setDoneTodos([]);
-  };
+  let reset = () => {};
+  let arr: any = [];
+  useEffect(() => {
+    let arr:any = [];
+    try {
+      getDocs(collection(db, "UserData")).then((QS) => {
+        QS.forEach((doc) => {
+          arr.push(doc.data());
+        });
+      });
+      setldg("Loading...");
+      setdata(arr)
+    } catch (error) {
+      alert(error)
+    }
+    finally{
+      setldg("Fecthed...");
 
-  let a  = () =>{
-    return new Date()
-  }
-let dt:object = a()
+    }
+  },[]);
   return (
     <div className={styles.parent}>
       <div className={styles.todo}>
         <div className={styles.manager}>
           <input
             onChange={(e) => {
-              setinput(e.target.value);
+              setUserInput({
+                id: data.length + 1,
+                description: e.target.value,
+              });
             }}
-            value={input}
+            value={userInput?.description}
             type="text"
             placeholder="Add A Task Here..."
           />
-          <span onClick={save}>+</span>
+          <button id="btn1" onClick={save}>
+            +
+          </button>
         </div>
         <div className={styles.todos}>
-          {data.map((item, i) => {
+          {data.map((item: any, i: any) => {
             return (
               <div key={i} className={styles.inmap}>
                 <div className={styles.intodop1}>
                   <div>
                     <input type="checkbox" className={styles.checkb} />
-                    <p>{item}</p>
+                    <p>{item?.description}</p>
                   </div>
                   <div>
-                    <Image src={img1} alt="xhr" width={20} height={20} />
-                    <Image src={img2} alt="xhr" width={20} height={20} />
+                    <Image
+                      src={img1}
+                      alt="xhr"
+                      width={20}
+                      height={20}
+                      onClick={() => {
+                        checked(item);
+                      }}
+                    />
+                    <Image
+                      src={img2}
+                      alt="xhr"
+                      width={20}
+                      height={20}
+                      onClick={() => {
+                        cancel(item);
+                      }}
+                    />
                   </div>
                 </div>
                 <div className={styles.intodop2}>
@@ -81,10 +118,10 @@ let dt:object = a()
               </div>
             );
           })}
-          {data.length === 0 ? <p>Add Some Task Now...</p> : <p></p>}
-          <hr className={styles.hr} />
+          {data.length === 0 ? <p>{ldg}</p> : <p>{ldg}</p>}
+          {/* <hr className={styles.hr} /> */}
 
-          {doneTodos.map((item, i) => {
+          {/* {doneTodos.map((item, i) => {
             return (
               <div
                 key={i}
@@ -111,7 +148,7 @@ let dt:object = a()
             <button onClick={reset}>Clear</button>
           ) : (
             <p></p>
-          )}
+          )} */}
         </div>
       </div>
     </div>
