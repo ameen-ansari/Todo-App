a1d80387588a36f0b8725f08678da09da60b3748
m.ameen <ameen55668@gmail.com>
2022-12-21 13:48:30 +0500

    test d

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,41 +1,44 @@
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import { collection, addDoc } from "firebase/firestore"; 
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import { collection, addDoc } from "firebase/firestore";
+import img1 from "/Images/edit.png";
+import img2 from "/Images/cancel (1).png";
+import Image from "next/image";
 
 export default function Home() {
-  const [data, setdata] = useState<string[]>([])
-  const [input, setinput] = useState<string>('')
-  const [doneTodos, setDoneTodos] = useState<string[]>([])
+  const [data, setdata] = useState<string[]>(["hallo this is my first task"]);
+  const [input, setinput] = useState<string>("");
+  const [doneTodos, setDoneTodos] = useState<string[]>([]);
 
   const save = () => {
     if (input.length > 0) {
-      let newdata: string[] = [...data, input]
-      setdata(newdata)
-      setinput('')
-    }else{
-      alert('Invalid')
+      let newdata: string[] = [...data, input];
+      setdata(newdata);
+      setinput("");
+    } else {
+      alert("Invalid");
     }
-  }
+  };
 
   let checked = (e: string) => {
-    let donedata: string[] = [...doneTodos, e]
-    setDoneTodos(donedata)
+    let donedata: string[] = [...doneTodos, e];
+    setDoneTodos(donedata);
     let filteredarr = data.filter((item) => {
-      return item != e
-    })
-    setdata(filteredarr)
-  }
+      return item != e;
+    });
+    setdata(filteredarr);
+  };
 
   let cancel = (e: string) => {
     let filteredarr = data.filter((item) => {
-      return item != e
-    })
-    setdata(filteredarr)
-  }
-  
+      return item != e;
+    });
+    setdata(filteredarr);
+  };
+
   let reset = () => {
-    setDoneTodos([])
-  }
+    setDoneTodos([]);
+  };
 
   return (
     <div className={styles.parent}>
@@ -43,7 +46,7 @@ export default function Home() {
         <div className={styles.manager}>
           <input
             onChange={(e) => {
-              setinput(e.target.value)
+              setinput(e.target.value);
             }}
             value={input}
             type="text"
@@ -54,14 +57,25 @@ export default function Home() {
         <div className={styles.todos}>
           {data.map((item, i) => {
             return (
-              <div key={i}>
-                <p>{item}</p>
-                <div>
-                  <span onClick={() => checked(item)}>&#10004;</span>
-                  <span onClick={() => cancel(item)}>&#10006;</span>
+              <div key={i} className={styles.inmap}>
+                <div className={styles.intodop1}>
+                  <div>
+                    <input type="checkbox" className={styles.checkb} />
+                    <p>{item}</p>
+                  </div>
+                  <div>
+                    <Image src={img1} alt="xhr" width={20} height={20} />
+                    <Image src={img2} alt="xhr" width={20} height={20} />
+                  </div>
+                </div>
+                <div className={styles.intodop2}>
+                  <div>
+                    <Image src={img1} alt="xhr" width={20} height={20} />
+                    <Image src={img2} alt="xhr" width={20} height={20} />
+                  </div>
                 </div>
               </div>
-            )
+            );
           })}
           {data.length === 0 ? <p>Add Some Task Now...</p> : <p></p>}
           <hr className={styles.hr} />
@@ -71,23 +85,23 @@ export default function Home() {
               <div
                 key={i}
                 style={{
-                  backgroundColor: 'rgb(92, 133, 217)',
-                  color: '#FFFFFF',
+                  backgroundColor: "rgb(92, 133, 217)",
+                  color: "#FFFFFF",
                 }}
               >
                 <p>{item}</p>
                 <div>
                   <span
                     style={{
-                      backgroundColor: '#FFFFFF',
-                      color: 'rgb(92, 133, 217)',
+                      backgroundColor: "#FFFFFF",
+                      color: "rgb(92, 133, 217)",
                     }}
                   >
                     &#10004;
                   </span>
                 </div>
               </div>
-            )
+            );
           })}
           {doneTodos.length !== 0 ? (
             <button onClick={reset}>Clear</button>
@@ -97,5 +111,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  )
+  );
 }
